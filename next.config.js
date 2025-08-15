d8e726076f1d9c03777c84a814cdc037f797f5a2
Adelina Figueira <adelinafigueiraf@gmail.com>
2025-08-15 14:31:50 -0400

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   distDir: 'build',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/adelina-figueira/' : '',
+  basePath: isProd ? '/adelina-figueira' : '',
+  output: 'export'
 };
 
 module.exports = nextConfig;
+
